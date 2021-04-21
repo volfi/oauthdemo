2a31c0a57cfd43d42cc3d93b74444f19ad05c760
Schwab, Wolfgang {DSGA~Penzberg} <wolfgang.schwab@roche.com>
2021-04-21 12:14:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Auth } from 'aws-amplify'
+
 function App() {
+    async function checkUser() {
+        const user = await Auth.currentAuthenticatedUser()
+        console.log('user: ' , user)
+    }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+            onClick = {() => Auth.federatedSignIn({provider : "Google"})}
+        >Sign in with Google</button>
+        <button
+            onClick = {() => Auth.federatedSignIn()}
+        >Sign in</button>
+          <button
+            onClick = {checkUser}
+        >Check User</button>
       </header>
     </div>
   );
